b5fcf603d2b507513036fc3ffa03c47858579661
Daria Kozina <kozina.flowers@yandex.ru>
2021-05-20 15:56:45 +0300

    corrected repair types data, fetch

--- src/layout/modules/showRepairTypesData.js
+++ src/layout/modules/showRepairTypesData.js
@@ -23,9 +23,60 @@ const showRepairTypesData = () => {
         return title.textContent;
     };
 
+
     document.addEventListener('click', event => {
         let text;
         const target = event.target;
+        const getData = () => {
+          fetch('../crm-backend/db.json')
+              .then(response => {
+                  if (response.status !== 200) {
+                      throw new Error('network status is not 200');
+                  }
+                  return (response.json());
+              })
+              .then(data => {
+                  namesArr = new Map();
+                  unitsArr = new Map();
+                  pricesArr = new Map();
+                  data.forEach((item, index) => {
+                      if (item.type === text) {
+                          countNames++;
+                          namesArr.set(countNames, item.name);
+                          unitsArr.set(countNames, item.units);
+                          pricesArr.set(countNames, item.cost);
+                      }
+                  })
+                  countNames = 0;
+              })
+              .then(() => {
+                  repairTypesNames.forEach((elem, index) => {
+                      if (index + 1 < namesArr.size) {
+                          elem.textContent = '';
+                          elem.parentNode.style.display = '';
+                      } else {
+                          elem.parentNode.style.display = 'none';
+                      }
+                  })
+                  namesArr.forEach((elem, index) => {
+                      repairTypesNames[index - 1].textContent = elem;
+                  });
+                  unitsArr.forEach((elem, index) => {
+                      const unitLetters = String(elem.match(/\D+/g));
+                      let unitNumbers = String(elem.match(/\d+/g));
+                      if (unitNumbers === 'null') {
+                          unitNumbers = '';
+                      }
+                      repairTypesValues[index - 1].innerHTML = `${unitLetters}<sup>${unitNumbers}</sup>`;
+                  });
+                  pricesArr.forEach((elem, index) => {
+                      prices[index - 1].textContent = `${elem} руб.`;
+                  });
+              })
+              .catch(error => {
+                  console.error(error);
+              })
+        };
         if (document.querySelector('#nav-arrow-popup-repair_left')) {
             const navList = document.querySelector('.nav-list-popup-repair');
 
@@ -43,60 +94,14 @@ const showRepairTypesData = () => {
         }
         if (popup.style.visibility === 'visible') {
             text = title.textContent;
+            getData();
             if (target.closest('.popup-repair-types-nav__item')) {
                 text = getActualText(target);
             }
         }
 
 
-        fetch('../crm-backend/db.json')
-            .then(response => {
-                if (response.status !== 200) {
-                    throw new Error('network status is not 200');
-                }
-                return (response.json());
-            })
-            .then(data => {
-                namesArr = new Map();
-                unitsArr = new Map();
-                pricesArr = new Map();
-                data.forEach((item, index) => {
-                    if (item.type === text) {
-                        countNames++;
-                        namesArr.set(countNames, item.name);
-                        unitsArr.set(countNames, item.units);
-                        pricesArr.set(countNames, item.cost);
-                    }
-                })
-                countNames = 0;
-            })
-            .then(() => {
-                repairTypesNames.forEach((elem, index) => {
-                    if (index + 1 < namesArr.size) {
-                        elem.textContent = '';
-                        elem.parentNode.style.display = '';
-                    } else {
-                        elem.parentNode.style.display = 'none';
-                    }
-                })
-                namesArr.forEach((elem, index) => {
-                    repairTypesNames[index - 1].textContent = elem;
-                });
-                unitsArr.forEach((elem, index) => {
-                    const unitLetters = String(elem.match(/\D+/g));
-                    let unitNumbers = String(elem.match(/\d+/g));
-                    if (unitNumbers === 'null') {
-                        unitNumbers = '';
-                    }
-                    repairTypesValues[index - 1].innerHTML = `${unitLetters}<sup>${unitNumbers}</sup>`;
-                });
-                pricesArr.forEach((elem, index) => {
-                    prices[index - 1].textContent = `${elem} руб.`;
-                });
-            })
-            .catch(error => {
-                console.error(error);
-            })
+
 
     });
 };
